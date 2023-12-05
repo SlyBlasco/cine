dbe5fa77c6ef188e39ebd3316674539665241d34
Luis Blasco <luism@LuxhPC>
2023-12-05 15:11:49 -0700

    Se agregaron los metodos update, delete, create en la clase clientes

--- src/mx/itson/cine/entidades/Cliente.java
+++ src/mx/itson/cine/entidades/Cliente.java
@@ -22,6 +22,7 @@ public class Cliente {
     private String apellido;
     private String email;
     
+    // Metodo que lee toda los datos de la tabla clientes y los agrega a una lista.
     public static List<Cliente> getAll(){
         List<Cliente> clientes = new ArrayList();
         try {
@@ -41,6 +42,66 @@ public class Cliente {
             System.err.print("Error:"+ e.getMessage());
         }
         return clientes;
+    }
+    
+    // Metodo que crea filas en la tabla clientes.
+    public static boolean create(String nombre, String email) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "INSERT INTO clientes(nombre, email) VALUES(? , ?)";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setString(1, nombre);
+            statement.setString(2, email);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
+
+    // Metodo que actualiza una de las filas de la tabla clientes.
+    public static boolean update(int id, String nombre, String email) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "UPDATE clientes SET nombre = ?, email = ? WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setString(1, nombre);
+            statement.setString(2, email);
+            statement.setInt(3, id);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
+
+    // Metodo que elimina una de las filas de la tabla clientes.
+    public static boolean delete(int id) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "DELETE clientes WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
     }
 
     public int getId() {
