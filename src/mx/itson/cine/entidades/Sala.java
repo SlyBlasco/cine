c7d00ec3c6ac2349f93888d072768d351765395d
Luis Blasco <luism@LuxhPC>
2023-12-05 15:26:44 -0700

    Se agregaron los metodos update, delete, create en la clase sala

--- src/mx/itson/cine/entidades/Sala.java
+++ src/mx/itson/cine/entidades/Sala.java
@@ -21,6 +21,7 @@ public class Sala {
     private String tipo;
     private int capacidad;
     
+    // Metodo que lee toda los datos de la tabla salas y los agrega a una lista.
     public static List<Sala> getAll() {
         List<Sala> salas = new ArrayList();
         try {
@@ -42,6 +43,65 @@ public class Sala {
         return salas;
     }
 
+    // Metodo que crea filas en la tabla salas.
+    public static boolean create(String tipo, String capacidad) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "INSERT INTO salas(tipo, capacidad) VALUES(? , ?)";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setString(1, tipo);
+            statement.setString(2, capacidad);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
+
+    // Metodo que actualiza una de las filas de la tabla salas.
+    public static boolean update(int id, String tipo, String capacidad) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "UPDATE salas SET tipo = ?, capacidad = ? WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setString(1, tipo);
+            statement.setString(2, capacidad);
+            statement.setInt(3, id);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
+
+    // Metodo que elimina una de las filas de la tabla salas.
+    public static boolean delete(int id) {
+        boolean result = false;
+        try {
+            Connection conexion = MySQLConnection.get();
+            String query = "DELETE from salas WHERE id = ?";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.execute();
+
+            result = statement.getUpdateCount() == 1;
+
+            conexion.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
 
     public int getId() {
         return id;
